c432ac095cdd30ef1d8ea1e6fdceab55105a4667
Wiktor Bober <135622933+MasterMind07777@users.noreply.github.com>
2025-01-15 17:17:19 +0100

    Car Class creation and bug fixing in Vehicle Class

---
+++ Car.py
@@ -0,0 +1,30 @@
+#Car
+
+from Vehicle import Vehicle
+
+class Car(Vehicle):
+    def __init__(
+        self,
+        plates: str,
+        brand: str = "Toyota",
+        model: str = "Yaris",
+        colour: str = "Yellow",
+        enginesize:int = 100,
+        fuelType: str = "Diesel",
+        passengersMax: int = 4,
+        passengersOnBoard: int = 10,
+        weightMax: int = 20,
+        wieghtTransported: int = 10):
+
+        super().__init__(plates, brand, model, colour, enginesize, fuelType)
+        
+        self.__passengersMax = passengersMax
+        self.__passengersOnBoard = passengersOnBoard
+
+    def __str__(self):
+        return __class__.__name__ + str(self.__dict__)
+    def __repr__(self):
+        return __class__.__name__ + str(self.__dict__)
+
+
+c = Car("AB123CD")
--- Vehicle.py
+++ Vehicle.py
@@ -1,14 +1,24 @@
 #Vehicle
 
-colourList = ["blu", "giallo", "nero"]
-brandList = ["audi", "nissan", "panda"]
-fuelList = ["diesel", "elettrico"]
+colourList = ["Blue", "Yellow", "Black"]
+brandList = ["Audi", "Nissan", "Panda", "Toyota"]
+fuelList = ["Diesel", "Electric"]
 letterList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ]
 numList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 import random
 
 class Vehicle:
-    def __init__(self, brand, model, colour, enginesize:int, fuelType, plates):
+    #Init
+    def __init__(
+        self,
+        plates: str,
+        brand: str = "Toyota",
+        model: str = "Yaris",
+        colour: str = "Yellow",
+        enginesize:int = 100,
+        fuelType: str = "Diesel"):
+
+        
         if brand not in brandList:
             raise ValueError("IMPOSSIBLE")
         else:
@@ -17,7 +27,7 @@ class Vehicle:
               raise ValueError("IMPOSSIBLE")
         else:  
             self.colour = colour
-        if enginesize < 0 and enginesize % 100 != 0:
+        if enginesize < 0 or enginesize % 100 != 0:
             raise ValueError("value not supported")
         else:
             self.enginesize = enginesize
@@ -25,11 +35,10 @@ class Vehicle:
             raise ValueError("IMPOSSIBLE")
         else:
             self.fuelType = fuelType
-        for x in plates:
-            if x[0] in letterList and x[1] in letterList and x[2] in numList and x[3] in numList and x[4] in numList and x[5] in letterList and x[6] in letterList:
-                self.plates = plates
-            else:
-                raise ValueError("plates not available")
+        if plates[0] in letterList and plates[1] in letterList and plates[2] in numList and plates[3] in numList and plates[4] in numList and plates[5] in letterList and plates[6] in letterList:
+            self.plates = plates
+        else:
+            raise ValueError("plates not available")
            
     def __lt__(self, other):
         if self.brand == other.brand:
@@ -39,3 +48,13 @@ class Vehicle:
                 return letterList.index(self.model.upper()[0]) < letterList.index(other.model.upper()[0])
         else:
             return letterList.index(self.brand.upper()[0]) < letterList.index(other.brand.upper()[0])
+
+    def __str__(self):
+        return __class__.__name__ + str(self.__dict__)
+    def __repr__(self):
+        return __class__.__name__ + str(self.__dict__)
+
+
+v = Vehicle("AB123CD")
+print(v)
+
